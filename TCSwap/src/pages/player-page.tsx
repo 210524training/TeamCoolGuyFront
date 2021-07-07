ee1cab524d3d51c1a7be6be9a65517b3deeceef7
adidde1073 <84734004+adidde1073@users.noreply.github.com>
2021-07-07 13:49:21 -0700

    Added rbare bone view store page

--- TCSwap/src/pages/player-page.tsx
+++ TCSwap/src/pages/player-page.tsx
@@ -5,8 +5,11 @@ import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
 import { loginAsync, logout, selectUser, UserState } from '../hooks/slices/user.slice'; */
 import { useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// import pages
 import TradePage from './trade-page';
 import MyCollectionPage from './player-colleciton-page';
+import ViewStoresPage from './stores-page';
 
 const PlayerPage: React.FC<unknown> = () => {
 
@@ -17,6 +20,7 @@ const PlayerPage: React.FC<unknown> = () => {
       <Tab.Navigator>
           <Tab.Screen name="Trades" component={TradePage} />
           <Tab.Screen name="My Collection" component={MyCollectionPage} />
+          <Tab.Screen name="View Stores" component={ViewStoresPage} />
       </Tab.Navigator>
     </>
   );
---
+++ TCSwap/src/pages/stores-page.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Button, StyleSheet, TextInput, Text, View } from 'react-native';
+/* import { useAppDispatch, useAppSelector } from '../hooks';
+import { loginAsync, logout, selectUser, UserState } from '../hooks/slices/user.slice'; */
+import { useNavigation } from '@react-navigation/native';
+
+const ViewStoresPage: React.FC<unknown> = () => {
+
+  return (
+    <View>
+      <Text>View stores here</Text>
+    </View >
+  );
+}
+
+export default ViewStoresPage;ViewStoresPage
