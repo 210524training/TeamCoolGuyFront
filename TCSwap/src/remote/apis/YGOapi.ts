6476bd1eb5faac94ee56dd4250a39d19e7f66391
revix4 <james.p1@verizon.net>
2021-07-07 20:50:29 -0400

    Created the flow for the player collection page

---
+++ TCSwap/src/remote/apis/YGOapi.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
+
+// You can create an axios configuration for specific remote APIs with baseline
+// configuration options
+// These can generally be overridden when sending individual requests
+const ygoClient = axios.create({
+  baseURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getCardByName = async (name: string): Promise<unknown> => {
+    const url = '?name=' + name;
+    const response = await ygoClient.get<unknown>(url);
+    return response.data;
+}
