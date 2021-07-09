3c65209c9ab73e995e21fddd6b09b223d736dfaa
charleshardy1 <charlesgoffhardy@gmail.com>
2021-07-08 18:15:34 -0700

    custom button matching style

---
+++ TCSwap/src/components/CustButton1.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+
+export default function CustButton1(props: { onPress: any; title?: string | undefined; }) {
+  const { onPress, title = 'Save' } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#731F17',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    letterSpacing: 0.25,
+    color: '#D98E04',
+  },
+});
