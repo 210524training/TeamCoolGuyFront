8122f3e2c259cf793f251b151e2edd6c52890f3c
charleshardy1 <charlesgoffhardy@gmail.com>
2021-07-08 18:15:45 -0700

    impl register button

--- TCSwap/src/pages/register-page.tsx
+++ TCSwap/src/pages/register-page.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Button, StyleSheet, TextInput, Text, View} from 'react-native';
+import { Button, StyleSheet, TextInput, Text, View, StatusBar} from 'react-native';
 import {Picker} from '@react-native-community/picker';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useAppDispatch, useAppSelector } from '../redux';
 import { loginAsync, logout, selectUser, UserState } from '../redux/slices/user.slice'; 
 import { useNavigation } from '@react-navigation/native';
+import CustButton1 from '../components/CustButton1';
 
 type Props = { navigation: any }
 
@@ -16,7 +17,7 @@ const LoginPage: React.FC<Props> = ({ navigation }) => {
   const [password, setPassword] = useState<string>();
   const [firstName, setFirstName] = useState<string>();
   const [lastName, setLastName] = useState<string>();
-  const [role, setRole] = useState<'player'|'store owner'| ''>('');
+  //const [role, setRole] = useState<'player'|'store owner'| ''>('');
   const dispatch = useAppDispatch();
 
 
@@ -38,10 +39,10 @@ const LoginPage: React.FC<Props> = ({ navigation }) => {
           alert('enter your last name');
           return;
       }
-      if(!role){
+      /*if(!role){
           alert('select your role');
           return;
-      }
+      }*/
 
       //login if register is successful
       const newUser={
@@ -49,7 +50,7 @@ const LoginPage: React.FC<Props> = ({ navigation }) => {
         password,
         firstname: firstName,
         lastname: lastName,
-        role
+        role: 'player'
       };
 
       // register user
@@ -59,34 +60,40 @@ const LoginPage: React.FC<Props> = ({ navigation }) => {
   return (
     <>
 
-      
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Register Here!</Text> 
+      </View>
       <View style={{ width: '100%', padding: 25, }}>
+        <Text >Username:</Text>
         <TextInput
-          style={{ fontSize: 18, margin: 10 }}
+          style={{ fontSize: 18, margin: 10, backgroundColor: 'white' }}
           placeholder="Username"
           onChangeText={text => setUsername(text)}
           defaultValue={username}
         />
+        <Text >Password:</Text>
         <TextInput
-          style={{ fontSize: 18, margin: 10 }}
+          style={{ fontSize: 18, margin: 10, backgroundColor: 'white'  }}
           placeholder="Password"
           secureTextEntry={true}
           onChangeText={text => setPassword(text)}
           defaultValue={password}
         />
+        <Text >First Name:</Text>
         <TextInput
-          style={{ fontSize: 18, margin: 10 }}
+          style={{ fontSize: 18, margin: 10, backgroundColor: 'white'  }}
           placeholder="First Name"
           onChangeText={text => setFirstName(text)}
           defaultValue={firstName}
         />
+        <Text >Last Name:</Text>
         <TextInput
-          style={{ fontSize: 18, margin: 10 }}
+          style={{ fontSize: 18, margin: 10, backgroundColor: 'white'  }}
           placeholder="Last Name"
           onChangeText={text => setLastName(text)}
           defaultValue={lastName}
         />
-        <Picker
+        {/*<Picker
         selectedValue={role}
         style={{ height: 50, width: 150 }}
         onValueChange={(val, itemIndex) => {if(val==='player'||val==='store owner'||val===''){setRole(val);}}}
@@ -94,16 +101,28 @@ const LoginPage: React.FC<Props> = ({ navigation }) => {
             <Picker.Item label="Select Role" value={''} />
             <Picker.Item label="Player" value="player" />
             <Picker.Item label="Store Owner" value="store owner" />
-        </Picker>
+        </Picker>*/}
 
-        <Button
-          onPress={handleRegister}
-          title="Register"
-        />
+        <CustButton1 onPress={handleRegister} title="Register"/>
       </View>
     </>
         
   );
 }
+
+const styles = StyleSheet.create ({
+  header: {
+    textAlign: 'center',
+    backgroundColor: '#731F17',
+    paddingTop: StatusBar.currentHeight
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 20,
+    color: '#D98E04',
+    textAlign: 'center'
+  }
+})
 
 export default LoginPage;
